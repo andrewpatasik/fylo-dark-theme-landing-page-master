fb2126e4ba0d22cc963ca1b14ed70cff6cff5d50
root <root@Andrew-PC>
2023-08-10 19:36:56 +0800

    global style and typography defined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Raleway } from 'next/font/google'
+
+const raleway = Raleway({ weight: ['400', '700'], subsets:['latin']});
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
     </main>
   )
 }

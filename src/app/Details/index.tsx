d50094b83bd4dfd79ec6a0eb95bc26e7ea73123b
root <andrew.yehezkiel17@gmail.com>
2023-08-17 12:23:42 +0800

    details desktop

--- src/app/Details/index.tsx
+++ src/app/Details/index.tsx
@@ -6,41 +6,48 @@ const raleway = Raleway({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const Details = () => {
   return (
-    <section className="relative text-left px-6 mb-20">
-      <div className="z-10 pt-3">
+    <section className="relative flex flex-col md:flex-row text-left px-6 md:px-16 my-20 md:space-x-16">
+      <div className="z-10 pt-3 md:basis-1/2">
         <Image
           src="/images/illustration-stay-productive.png"
           alt="illustration-stay-productive"
           width={0}
           height={0}
           sizes="100vw"
-          className="mx-auto w-full h-auto"
+          className="w-full h-auto"
         />
       </div>
-      <h3 className={`${raleway.className} text-xl font-bold mt-12 mb-4`}>
-        Stay productive, wherever you are
-      </h3>
-      <div className="tracking-tighter flex flex-col space-y-4">
-        <p>
-          Never let location be an issue when accessing your files. Fylo has you
-          covered for all of your life storage needs.
-        </p>
-        <p>
-          Secure share files and folders with friends, family and colleagues for
-          live collaboration. No email attachments required.
-        </p>
+      <div className="flex md:flex-col md:space-y-4 md:px-16">
+        <h3
+          className={`${raleway.className} text-xl md:text-4xl font-bold mt-12 mb-4`}
+        >
+          Stay productive,<span className="md:block md:mt-2">wherever you are</span>
+        </h3>
+        <div className="tracking-tighter md:tracking-normal flex flex-col space-y-4">
+          <p>
+            Never let location be an issue when accessing your files. Fylo has
+            you covered for all of your life storage needs.
+          </p>
+          <p>
+            Secure share files and folders with friends, family and colleagues
+            for live collaboration. No email attachments required.
+          </p>
+        </div>
+        <Link
+          href="#"
+          className="flex items-center space-x-2 tracking-thighter border-b-2 w-fit pb-1 text-[#65E2D9] border-[#65E2D9]"
+        >
+          <p>See how Fylo works</p>
+          <Image
+            src="/images/icon-arrow.svg"
+            alt="illustration-stay-productive"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-5 h-5"
+          />
+        </Link>
       </div>
-      <Link href="#" className="flex items-center space-x-2 tracking-thighter border-b-2 w-fit pb-1 text-[#65E2D9] border-[#65E2D9]">
-        <p>See how Fylo works</p>
-        <Image
-          src="/images/icon-arrow.svg"
-          alt="illustration-stay-productive"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-5 h-5"
-        />
-      </Link>
     </section>
   );
 };
